22704720bfe8063ebb7400623a20cd5540bea40c
FilipMitrovic <filipmitrovic1902@gmail.com>
2023-12-15 20:32:18 +0100

    Napravljeno citanje fajlova
    Prvi korak i prvi pokusaj citanja fajla.

---
+++ Projekat ERS/Projekat ERS/CitanjeClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Projekat_ERS 
+{
+    class CitanjeClass : CitanjeFajlova 
+    {
+        PROGNOZIRANI_LOAD PL;
+        void CitanjeFajlova.Citanje()
+        {
+            XmlSerializer serz = new XmlSerializer(typeof(PROGNOZIRANI_LOAD));
+
+            FileStream read = File.OpenRead("ostv_2020_05_07.xml");
+            var result = (PROGNOZIRANI_LOAD)(serz.Deserialize(read));
+        }
+    }
+}
---
+++ Projekat ERS/Projekat ERS/PROGNOZIRANI_LOAD.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekat_ERS
+{
+    class PROGNOZIRANI_LOAD
+    {
+        private int sat;
+        private int load;
+        private string oblast;
+
+        public PROGNOZIRANI_LOAD()
+        {
+            this.Sat = 0;
+            this.Load = 0;
+            this.Oblast = "";
+        }
+
+        public PROGNOZIRANI_LOAD(int sat, int load, string oblast)
+        {
+            this.Sat = sat;
+            this.Load = load;
+            this.Oblast = oblast;
+        }
+
+        public int Sat { get => sat; set => sat = value; }
+        public int Load { get => load; set => load = value; }
+        public string Oblast { get => oblast; set => oblast = value; }
+
+
+    }
+
+
+}
