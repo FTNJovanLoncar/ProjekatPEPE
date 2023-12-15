557ad8f8b58ef29b3f8811fad9e42a2983bf3b07
FTNJovanLoncar <148216615+FTNJovanLoncar@users.noreply.github.com>
2023-12-15 20:59:01 +0100

    Dodata lista

--- Projekat ERS/Projekat ERS/CitanjeClass.cs
+++ Projekat ERS/Projekat ERS/CitanjeClass.cs
@@ -11,12 +11,25 @@ namespace Projekat_ERS
     class CitanjeClass : CitanjeFajlova 
     {
         PROGNOZIRANI_LOAD PL;
+        public bool EndOfStream { get; }
+        IList<PROGNOZIRANI_LOAD> lista = new List<PROGNOZIRANI_LOAD>();
+        
         void CitanjeFajlova.Citanje()
         {
             XmlSerializer serz = new XmlSerializer(typeof(PROGNOZIRANI_LOAD));
 
             FileStream read = File.OpenRead("ostv_2020_05_07.xml");
-            var result = (PROGNOZIRANI_LOAD)(serz.Deserialize(read));
+            StreamReader reader = new StreamReader(read);
+            
+            while (!reader.EndOfStream) {
+
+                var result = (PROGNOZIRANI_LOAD)(serz.Deserialize(read));
+                PL = result;
+                lista.Add(PL);
+            }
         }
+
+
+        
     }
 }
